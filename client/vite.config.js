ad907c7e16945adf6790947ce8c0e8bbe87a64b3
yangdengcheng <yangdengcheng@linyang.com.cn>
2026-08-03 17:16:23 +0800

    build(client): 调整vite代理的后端端口至5180

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": { target: "http://localhost:3000", changeOrigin: true },
-      "/uploads": { target: "http://localhost:3000", changeOrigin: true },
+      "/api": { target: "http://localhost:5180", changeOrigin: true },
+      "/uploads": { target: "http://localhost:5180", changeOrigin: true },
     },
   },
 });
